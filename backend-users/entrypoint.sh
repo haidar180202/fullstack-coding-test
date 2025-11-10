eb7193b480d63641eb58b85298bf1e822ea14d55
haidar180202 <gshaidar6@gmail.com>
2025-11-10 21:03:08 +0700

    chore(fullstack-coding-test): update for error in backend-users

--- backend-users/entrypoint.sh
+++ backend-users/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+composer install --no-interaction --optimize-autoloader --no-dev
+
 # Wait for the database to be ready
 # This is a simple loop, a more robust solution might use docker-compose depends_on with healthchecks
 # or a tool like wait-for-it.sh. For this case, we assume the DB is quick to start.
